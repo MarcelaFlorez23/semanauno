2b719620f154132052e1ebe093f615eceac2af90
MarcelaFlorez23 <marce.florez2316@gmail.com>
2019-12-17 13:37:14 -0600

    semanauno

---
+++ src/semana1_prim/Semana1_Prim.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semana1_prim;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Marcela Florez
+ */
+public class Semana1_Prim {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num = 6;
+        int num2 = 3;
+        int suma =0;
+        int resta=0;
+        int multiplicacion=0;
+        int divicion=0;
+        suma = num+num2;
+        resta = num - num2;
+        multiplicacion= num*num2;
+        divicion = num/num2;
+        System.out.println();
+        System.out.println("La suma de los numeros es: " +suma);
+        System.out.println();
+        System.out.println("La resta de los numeros es: " +resta);
+        System.out.println();
+        System.out.println("La multiplicacion de los numeros es: " +multiplicacion);
+        System.out.println();
+        System.out.println("La divicion de los numeros es: " + divicion);
+        
+                   
+    }
+    }
+    
